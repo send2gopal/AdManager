b6f265e9ea67a2661dbd45441550d51dec0a6fba
Gopal Kumar Thakur <send2gopal@gmail.com>
2017-05-20 17:59:55 +0530

    - Added initial projects

---
+++ AdManager.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdManager.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ AdManager.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AdManager.Service
+{
+    public class Class1
+    {
+    }
+}
